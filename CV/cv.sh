950e9b18dad5eedb403a8d08cb6ec2efdc7c6754
yangju99 <didwndud3299@naver.com>
2025-06-11 23:38:41 +0900

    final

--- CV/cv.sh
+++ CV/cv.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-# gnome-terminal로 각각의 명령을 로그인 셸처럼 실행
-gnome-terminal -- bash -c "source ~/.bashrc; ros2 launch turtlebot3_gazebo turtlebot3_world.launch.py; exec bash"
+# shell 1: Gazebo 시뮬레이터 실행
+gnome-terminal -- bash -c "export FORCE_BASHRC=1; source ~/.bashrc; ros2 launch turtlebot3_gazebo turtlebot3_world.launch.py; exec bash"
 sleep 5  # Gazebo가 충분히 로딩될 시간
 
-gnome-terminal -- bash -c "source ~/.bashrc; ros2 launch turtlebot3_navigation2 navigation2.launch.py use_sim_time:=True map:=$HOME/map.yaml; exec bash"
+# shell 2: Navigation2 실행
+gnome-terminal -- bash -c "export FORCE_BASHRC=1; source ~/.bashrc; ros2 launch turtlebot3_navigation2 navigation2.launch.py use_sim_time:=True map:=$HOME/map.yaml; exec bash"
 sleep 5  # nav2 bringup 시간 확보
 
-gnome-terminal -- bash -c "source ~/.bashrc; python3 \$HOME/single_goal_nav.py; exec bash"
+# shell 3: 자동 목표 이동 Python 스크립트 실행
+gnome-terminal -- bash -c "export FORCE_BASHRC=1; source ~/.bashrc; python3 \$HOME/single_goal_nav.py; exec bash"
+
 
 
 
